250710005fc85b73586394d0ef6bada21eb7dd31
Cristian González <cgonzalez@tiempodevelopment.com>
2017-07-06 02:14:43 -0500

    Cortana Emotions API Demo

---
+++ Xamarin/CortanaDemo/CortanaDemo/CortanaDemo/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CortanaDemo.Helpers
+{
+    public static class Constants
+    {
+        public static readonly string EmotionApiKey = "d364610867d3433793af0c200f54cf74";
+    }
+}
---
+++ Xamarin/CortanaDemo/CortanaDemo/CortanaDemo/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Forms;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using CortanaDemo.Helpers;
+
+namespace CortanaDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        EmotionServiceClient emotionClient;
+        MediaFile photo;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            emotionClient = new EmotionServiceClient(Constants.EmotionApiKey);
+            activityIndicator.IsRunning = false;
+        }
+
+        private async void OnTakePhotoButtonClicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            // Take photo
+            if (CrossMedia.Current.IsCameraAvailable || CrossMedia.Current.IsTakePhotoSupported)
+            {
+                photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    Name = "emotion.jpg",
+                    PhotoSize = PhotoSize.Small
+                });
+
+                if (photo != null)
+                {
+                    image.Source = ImageSource.FromStream(photo.GetStream);
+                }
+            }
+            else
+            {
+                await DisplayAlert("No Camera", "Camera unavailable.", "OK");
+            }
+
+            ((Button)sender).IsEnabled = false;
+            activityIndicator.IsRunning = true;
+
+            // Recognize emotion
+            try
+            {
+                if (photo != null)
+                {
+                    using (var photoStream = photo.GetStream())
+                    {
+                        Emotion[] emotionResult = await emotionClient.RecognizeAsync(photoStream);
+                        if (emotionResult.Any())
+                        {
+                            // Emotions detected are happiness, sadness, surprise, anger, fear, contempt, disgust, or neutral.
+                            emotionResultLabel.Text = emotionResult.FirstOrDefault().Scores.ToRankedList().FirstOrDefault().Key;
+                        }
+                        photo.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            activityIndicator.IsRunning = false;
+            ((Button)sender).IsEnabled = true;
+        }
+    }
+}
